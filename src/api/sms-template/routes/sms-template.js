5a3dbbc48fb249a60b68ff7ad2a8fa7890ade72b
codehubslgit <devcodehubsl@gmail.com>
2023-07-18 12:10:41 +0000

    changes-18072023

---
+++ src/api/sms-template/routes/sms-template.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sms-template router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::sms-template.sms-template');
