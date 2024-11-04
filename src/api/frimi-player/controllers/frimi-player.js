f9a792137d7bab55107e164f77bb0f6884fa2a96
Navod Amarakoon <nexza@Users-MacBook-3.local>
2024-11-04 21:24:32 +0530

    FRIMI INTIGRATION

---
+++ src/api/frimi-player/controllers/frimi-player.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * frimi-player controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::frimi-player.frimi-player');
---
+++ src/api/frimi-player/routes/frimi-player.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * frimi-player router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::frimi-player.frimi-player');
