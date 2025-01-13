0998b4fb613283d90ef3416cd3dfeea6b9410481
nexzanavod <aanavod@gmail.com>
2025-01-13 11:46:15 +0530

    add frimi transaction

---
+++ src/api/frimi-player/common/constants.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+    HEADERS: {
+        CHANNEL: '2',
+        VERSION: '1',
+        REQUEST_TYPE: 'LS',
+        LANGUAGE_CODE: 'en',
+    },
+    CURRENCY_CODES: {
+        LKR: '144',
+    },
+    RESPONSE_SUCCESS: 'Success',
+};
